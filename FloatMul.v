b65d7426cf4100ef08d6d8f8d3340727f989830f
Avik Trilok <109292740+avik-trilok-12@users.noreply.github.com>
2024-08-17 00:08:26 +0530

    Update FloatMul.v

--- FloatMul.v
+++ FloatMul.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module FloatingMultiplication #(parameter XLEN=32)
+module FloatMul #(parameter XLEN=32)
                                 (input [XLEN-1:0]A,
                                  input [XLEN-1:0]B,
                                  input clk,
